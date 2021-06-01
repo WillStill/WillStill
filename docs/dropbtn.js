9ff3ab083e1cea306b3d0084e810610ddc71c475
WillStill <wxs5242@psu.edu>
2021-06-01 17:54:01 -0400

    Completed mobile use of projects page. Used Intersection Observer API. Bugs can be solved another time.

--- docs/dropbtn.js
+++ docs/dropbtn.js
@@ -1,12 +1,13 @@
 window.onload = function transform(){
+    var btn = document.getElementById("tbtnbox")
     document.querySelector('.dropbtn').onclick = function(){
         if(this.className.match('dropbtn_clicked')) {
             this.className = 'dropbtn'; //transform the dropbutton
-            document.getElementById("tbtnbox").style.visibility = "hidden"; //hide and show another element (tbtnbox)
+            btn.style.visibility = "hidden"; //hide and show another element (tbtnbox)
         }
         else {
             this.className = 'dropbtn dropbtn_clicked';
-            document.getElementById("tbtnbox").style.visibility = "visible";
+            btn.style.visibility = "visible";
             
         }
     };
---
+++ docs/interobser.js
@@ -0,0 +1,18 @@
+const options = {
+  threshold: [0.8]
+};
+const target = document.querySelectorAll('.observe');
+
+function handleIntersection(entries) {
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  });
+}
+const observer = new IntersectionObserver(handleIntersection, options);
+target.forEach(target => {
+  observer.observe(target, options);
+}, options);
